20b5855b00e6fa2dab44dca4700d9ad22bf4f52f
Joao Vitor <joaovlopes000@gmail.com>
2023-04-25 19:05:37 -0300

    - add POST mapping to Beer

--- mssc-brewery/src/main/java/com/jojo/msscbrewery/web/controller/BeerController.java
+++ mssc-brewery/src/main/java/com/jojo/msscbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package com.jojo.msscbrewery.web.controller;
 
 import com.jojo.msscbrewery.services.BeerService;
 import com.jojo.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -25,5 +23,15 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable UUID beerId) {
 
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(BeerDto beerDto) {
+        BeerDto savedBeerDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer" + savedBeerDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 }
